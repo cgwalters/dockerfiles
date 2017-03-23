5a82a2201311d0d519f850a5232d788338761fb6
Colin Walters <walters@verbum.org>
2017-03-23 10:43:15 -0400

    fdev: Misc fixes

--- fdev/base.sh
+++ fdev/base.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -xeuo pipefail
 dnf -y install yum-utils tmux sudo powerline \
-    gcc redhat-rpm-config make mock fedpkg \
+    gcc clang redhat-rpm-config make mock fedpkg \
     libvirt libguestfs-tools libguestfs-xfs \
     virt-install curl git kernel hardlink \
     git-evtag {python3-,}dnf-plugins-core \
-    jq powerline
+    jq powerline gdb
 dnf builddep -y glib2 systemd ostree rpm-ostree
