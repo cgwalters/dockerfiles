fe286fcdbb6f1cd2cc8d1b4547e68b6b739d09d6
Colin Walters <walters@verbum.org>
2017-03-27 10:57:29 -0400

    fdev: Add strace

--- fdev/base.sh
+++ fdev/base.sh
@@ -2,7 +2,7 @@
 set -xeuo pipefail
 dnf -y install yum-utils tmux sudo powerline \
     gcc clang redhat-rpm-config make mock fedpkg \
-    libvirt libguestfs-tools libguestfs-xfs \
+    libvirt libguestfs-tools strace libguestfs-xfs \
     virt-install curl git kernel hardlink \
     git-evtag {python3-,}dnf-plugins-core \
     jq powerline gdb
