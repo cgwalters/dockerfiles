40ad391d5b21b58355fba3b3fa99588f73a7afe0
Colin Walters <walters@verbum.org>
2017-11-16 13:07:36 -0500

    fdev: install rpm-ostree

--- fdev/base.sh
+++ fdev/base.sh
@@ -5,6 +5,6 @@ yum -y install bash-completion yum-utils tmux sudo powerline \
     libvirt libguestfs-tools strace libguestfs-xfs \
     virt-install curl git kernel hardlink rsync \
     git-evtag {python3-,}dnf-plugins-core \
-    origin-clients \
+    origin-clients ostree{,-grub2} rpm-ostree \
     jq powerline gdb
 dnf builddep -y glib2 systemd ostree rpm-ostree
