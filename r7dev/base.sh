cf828d6886d2d63a8cc267089b978420515ab152
Colin Walters <walters@verbum.org>
2017-12-06 10:20:05 -0500

    r7dev: Sync with fdev (use --init)
    Also drop gpgcheck due to unsigned packages (sigh).

--- r7dev/base.sh
+++ r7dev/base.sh
@@ -3,22 +3,22 @@ set -xeuo pipefail
 cat > /etc/yum.repos.d/rhel-server-internal.repo << EOF
 [rhel7-server]
 baseurl=http://download-node-02.eng.bos.redhat.com/nightly/latest-RHEL-7/compose/Server/x86_64/os/
-gpgcheck=1
+gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 enabled=1
 [rhel7-server-source]
 baseurl=http://download-node-02.eng.bos.redhat.com/nightly/latest-RHEL-7/compose/Server/source/tree/
-gpgcheck=1
+gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 enabled=0
 [rhel7-server-optional]
 baseurl=http://download-node-02.eng.bos.redhat.com/nightly/latest-RHEL-7/compose/Server-optional/x86_64/os/
-gpgcheck=1
+gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 enabled=1
 [rhel7-server-extras]
 baseurl=http://download-node-02.eng.bos.redhat.com/nightly/EXTRAS-RHEL-7.4/latest-EXTRAS-7-RHEL-7/compose/Server/x86_64/os/
-gpgcheck=1
+gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 enabled=1
 EOF
